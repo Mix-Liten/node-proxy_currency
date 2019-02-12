dadf03b214117e5eb5ffa61870c8e07f302bcdfd
ianchang <ian.chang@dev.cctech-support.com>
2019-02-12 13:13:29 +0800

    Filter data

--- routes/currency.js
+++ routes/currency.js
@@ -4,17 +4,28 @@ var router = express.Router();
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
-router.get('/', function(req, res, next) {
-    let queries=req.query
-    console.log('query: ', queries);
-    let url='https://tw.rter.info/capi.php'
-    var options = {
-        host: url,
-        method: 'GET'
-      };
-    
-    client.get(url, function (data, response) { 
-        res.json({...data})
-    });
+const needsList = ["USDUSD", "USDHKD", "USDGBP", "USDAUD", "USDCAD", "USDSGD", "USDCHF", "USDJPY", "USDZAR", "USDSEK", "USDNZD", "USDTHB", "USDPHP", "USDIDR", "USDEUR", "USDKRW", "USDVND", "USDMYR", "USDCNY"]
+
+router.get('/', function (req, res, next) {
+  let queries = req.query
+  let url = 'https://tw.rter.info/capi.php'
+  var options = {
+    host: url,
+    method: 'GET'
+  };
+
+  client.get(url, function (datas, response) {
+    let dataObject = {}
+    const valueList = Object.values(datas)
+    Object.keys(datas).forEach((data, index, arr) => {
+      if (needsList.find(key => data === key)) {
+        dataObject[data] = valueList[index]
+      }
+    })
+
+    res.json({
+      ...dataObject
+    })
+  });
 });
 module.exports = router;
